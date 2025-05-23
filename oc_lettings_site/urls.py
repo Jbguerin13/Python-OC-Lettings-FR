d84c8c7ea2bfd12a580e755667a6eef2d3a34c2e
jean-baptiste.guerin <jean-baptiste.guerin@jellyfish.com>
2025-05-23 15:00:36 +0200

    feat: error 404 and 500 templates

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -7,4 +10,7 @@ urlpatterns = [
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
-]
+    # Test URLs for error pages
+    path('404/', views.test_404, name='test_404'),
+    path('500/', views.test_500, name='test_500'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
