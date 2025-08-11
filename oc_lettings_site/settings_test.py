0fde1b3e866cf11d26f1a9b474cd685121be596d
jean-baptiste.guerin <jean-baptiste.guerin@jellyfish.com>
2025-08-11 16:41:21 +0200

    fix:test error module import six

--- oc_lettings_site/settings_test.py
+++ oc_lettings_site/settings_test.py
@@ -11,6 +11,9 @@ DATABASES = {
 # Disable debug mode for tests
 DEBUG = False
 
+# Set a default SECRET_KEY for CI/CD tests
+SECRET_KEY = 'test-secret-key-for-ci-cd-pipeline-12345'
+
 # Use a fast password hasher for tests
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
